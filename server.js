d16f5ce75c9bffb6264c754a3b95208a7e66d538
Piyush Tiwari <ithastobepiyush@gmail.com>
2026-04-13 11:46:57 +0530

    trending blog layout

--- server.js
+++ server.js
@@ -295,6 +295,22 @@ server.get('/latest-blogs', (req, res) => {
     })
 })
 
+// TRENDING BLOG
+server.get("/trending-blogs", (req, res) => {
+
+    Blog.find({ draft: false})
+    .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id")
+    .sort({"activity.total_read": -1, "activity.total_likes": -1, "publishedAt": -1})
+    .select("blog_id title publishedAt -_id")
+    .limit(5)
+    .then(blogs => {
+        return res.status(200).json({ blogs})
+    })
+    .catch(err => {
+        return res.status(500).json({ error: err.message})
+    })
+})
+
 // blog create route with  validation to save datat
 server.post('/create-blog', verifyJWT, (req, res) => {
 
